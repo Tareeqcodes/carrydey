97b5ad69ef59f1c64b31c7c9447edbf73948d0e0
Devtareeq <101841038+Tareeqcodes@users.noreply.github.com>
2025-07-22 12:38:23 +0100

    added status an match on navbar

--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -1,6 +1,6 @@
 'use client';
 import Link from 'next/link';
-import { User, Search, Plus, Wallet, Settings } from 'lucide-react';
+import { User, Search, Settings, Package, Info } from 'lucide-react';
 export default function Navbar() {
   return (
      <div className="fixed bottom-0 left-0 right-0 bg-white border-t border-gray-200 px-4 py-2">
@@ -23,23 +23,23 @@ export default function Navbar() {
            
             <Search size={24} className="relative z-10" />
           </div>
-          <span className="text-xs font-medium">Search</span>
+          <span className="text-xs font-medium">Browse</span>
         </button>
         
         <button 
           className="flex flex-col items-center space-y-1 p-2 rounded-lg text-white bg-blue-600 hover:bg-blue-700 transition-colors"
         >
-          <Plus size={24} />
-          <span className="text-xs font-medium">Post</span>
+          <Package size={24} />
+          <span className="text-xs font-medium">Match</span>
         </button>
         
         <button 
           className='flex flex-col items-center space-y-1 p-2 rounded-lg transition-colors'
         >
           <div className="relative">
-            <Wallet size={24} className="relative z-10" />
+            <Info size={24} className="relative z-10" />
           </div>
-          <span className="text-xs font-medium">Wallet</span>
+          <span className="text-xs font-medium">Status</span>
         </button>
           <Link href="/setting">
         <button 
