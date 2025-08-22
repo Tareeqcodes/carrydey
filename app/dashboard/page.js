f6e1ee03fbc2e06ce81253800159ba714b496ad3
Devtareeq <101841038+Tareeqcodes@users.noreply.github.com>
2025-08-22 10:14:09 +0100

    added package detail page

--- app/dashboard/page.js
+++ app/dashboard/page.js
@@ -1,12 +1,11 @@
 "use client";
-import Link from "next/link"
-import PackageFeed from "@/components/PackageFeed"
+import Link from "next/link";
 import Main from "@/components/Main";
 import Hero from "@/components/Hero";
 import { UserRole } from "@/hooks/UserRole";
-import QuickNav from "@/components/QuickNav"
+import QuickNav from "@/components/QuickNav";
 import Navbar from "@/components/Navbar";
-import { useAuth } from "@/hooks/Authcontext"
+import { useAuth } from "@/hooks/Authcontext";
 import ContentLoading from "@/components/ui/ContentLoading";
 import PendingVerification from "../travelerdashboard/page";
 
@@ -17,19 +16,19 @@ export default function page() {
   if (loading && role === "sender") {
     return <ContentLoading />;
   }
- 
+
   return (
     <>
       {user ? (
         <div>
           {role === "sender" ? (
             // Sender Dashboard
-            <div className="mb-20 bg-gradient-to-br from-gray-50 to-blue-50 px-6">
+            <div className='mb-20 bg-gradient-to-br from-gray-50 to-blue-50 px-6'>
               <Main role={role} name={name} />
               <Hero role={role} />
-              <PackageFeed />
+
               <QuickNav />
-              <div className="h-20"></div>
+              <div className='h-20'></div>
             </div>
           ) : (
             <div>
@@ -39,13 +38,13 @@ export default function page() {
           <Navbar />
         </div>
       ) : (
-        <div className="flex flex-col items-center justify-center h-screen">
-          <h1 className="text-2xl font-bold">Please log in to continue</h1>
-          <Link href="/login" className="mt-4 text-blue-500">
+        <div className='flex flex-col items-center justify-center h-screen'>
+          <h1 className='text-2xl font-bold'>Please log in to continue</h1>
+          <Link href='/login' className='mt-4 text-blue-500'>
             Go to Login
           </Link>
         </div>
       )}
     </>
-  )
+  );
 }
---
+++ components/PackageInfo.jsx
@@ -0,0 +1,7 @@
+import React from 'react'
+
+export default function PackageInfo() {
+  return (
+    <div>PackageInfo</div>
+  )
+}
