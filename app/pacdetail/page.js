0928ed4b95697d4cf9b1751bc923aa580f092896
Tareeqcodes <tariqauwal020@gmail.com>
2025-08-30 11:38:54 +0100

    added package details page

--- app/pacdetail/page.js
+++ app/pacdetail/page.js
@@ -1,7 +1,28 @@
-import React from 'react'
+import { useParams } from "next/navigation"
+import { useEffect, useState } from "react"
+import GetSinglePackage from "@/hooks/GetSinglePackage"
+
 
 export default function page() {
-  return (
-    <div>page</div>
+  const {id} = useParams()
+  const [data, setData] = useState(null)
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const result = await GetSinglePackage(id)
+        setData(result)
+      } catch (error) {
+        console.error("Error fetching package data:", error)
+      }
+    }
+    fetchData()
+  }, [id])
+
+  if (!data) {
+    return <div>Loading...</div>
+  }
+  return ( 
+    <p>hello</p>
   )
 }
---
+++ hooks/GetSinglePackage.js
@@ -0,0 +1,17 @@
+import { databases } from "@/lib/config/Appwriteconfig"
+
+
+const db = process.env.NEXT_PUBLIC_APPWRITE_DATABASE_ID;
+const collection = process.env.NEXT_PUBLIC_APPWRITE_PACKAGE_COLLECTION_ID;
+
+const GetSinglePackage = async (id) => {
+  try {
+    const response = await databases.getDocument(db, collection, id);
+    return response;
+  } catch (error) {
+    console.error("Error fetching package:", error);
+    throw error;
+  }
+};
+
+export default GetSinglePackage;
